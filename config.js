cfa81f35883d1882c83fe9f9446d2e996e1104f7
mhpolh <47925223+mhpolh@users.noreply.github.com>
2019-02-26 22:31:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    TOKEN: 'Your bot token here', 
-    YT_API_KEY: 'Youtube API Key here', 
-    prefix: 'prefix here',
-    devs: ['your id/developers here']
+    TOKEN: 'NTUwMDUwNTk2NTc3ODY5ODU0.D1cyHA.RE7Hyr48ZeGU2XsZ2N5qEYsB5pA', 
+    YT_API_KEY: 'AIzaSyCQapWI28_Um0n4AEGNVw7l3QZqhQF0cFs', 
+    prefix: 'mhelp',
+    devs: ['476958270175838222']
 }
